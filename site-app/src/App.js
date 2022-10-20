94823e23d02cacbb274aafed17d868df437bd4fe
zz3553 <zz3553@g.rit.edu>
2022-10-20 12:12:24 -0400

    working on fixing picture for header

---
+++ site-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import profPic from './resources/prof.jpeg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img className="header-img" src={profPic} alt="" /> */}
+        <div class="header-page-links">
+          <p>
+            Projects
+          </p>
+          <p>
+            Work Experience
+          </p>
+          <p>
+            Hobbies
+          </p>
+          <p>
+            Contact Me
+          </p>
+        </div>
+      </header>
+      <div className="home-body">
+      </div>
+    </div>
+  );
+}
+
+export default App;
