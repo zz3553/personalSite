0d3d5326e58c8a0a14ce854199ff248eaee24b21
zz3553 <zz3553@g.rit.edu>
2022-10-20 13:14:50 -0400

    fixing picture position

--- site-app/src/App.js
+++ site-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img className="header-img" src={profPic} alt="" /> */}
+        <img className="header-img" src={profPic} alt="" />
         <div class="header-page-links">
           <p>
             Projects
